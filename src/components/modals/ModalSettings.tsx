a882862e162044071d4a3b2566408ae15c46fd3c
= <=>
2023-03-01 14:44:18 +0100

    initialize2

---
+++ src/components/modals/ModalSettings.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ModalSettings() {
+  return (
+    <div>ModalSettings</div>
+  )
+}
---
+++ src/components/modals/ModalStatistics.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ModalStatistics() {
+  return (
+    <div>ModalStatistics</div>
+  )
+}
